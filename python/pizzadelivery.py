489d3f3037b1ec859cb8be3069f13a62d39cf16c
Venkat <guptamaddula9@gmail.com>
2026-07-26 16:43:42 +0530

    simplest way

--- python/pizzadelivery.py
+++ python/pizzadelivery.py
@@ -1,65 +1,26 @@
 print("Welcome to the YoursPizza")
 size = input("What size pizza do you want? S, M, or L ")
+pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
 
-
-small_pizza = 15
-medium_pizza = 20
-large_pizza = 25
 
 if size == "S":
-    pepparoni = input("Do you want pepparoni? Y or N ")
-    if pepparoni == "Y":
-        bill=small_pizza+2
-        Extra_cheese = input("Do you want extra cheese? Y or N ")
-        if Extra_cheese == "Y":
-            bill=bill+1
-            print(f"Your total bill is: Rs{bill}")
-       
-    else:
-        Extra_cheese = input("Do you want extra cheese? Y or N ")
-        if Extra_cheese == "Y":
-            bill=small_pizza+1
-            print(f"Your total bill is: Rs{bill}")
-        else:
-            bill=small_pizza
-            print(f"Your total bill is: Rs{bill}")
-
+    bill = 15
 elif size == "M":
-    pepparoni = input("Do you want pepparoni? Y or N ")
-    if pepparoni == "Y":
-        bill=medium_pizza+3
-        Extra_cheese = input("Do you want extra cheese? Y or N ")
-        if Extra_cheese == "Y":
-            bill=bill+1
-            print(f"Your total bill is: Rs{bill}")
-        print(f"Your total bill is: Rs{bill}")
-    else:
-        Extra_cheese = input("Do you want extra cheese? Y or N ")
-        if Extra_cheese == "Y":
-            bill=medium_pizza+1
-            print(f"Your total bill is: Rs{bill}")
-        else:
-            bill=medium_pizza
-            print(f"Your total bill is: Rs{bill}")
-
+    bill = 20   
 elif size == "L":
-    pepparoni = input("Do you want pepparoni? Y or N ")
-    if pepparoni == "Y":
-        bill=large_pizza+3
-        Extra_cheese = input("Do you want extra cheese? Y or N ")
-        if Extra_cheese == "Y":
-            bill=bill+1
-            print(f"Your total bill is: Rs{bill}")
-        print(f"Your total bill is: Rs{bill}")
-    else:
-        Extra_cheese = input("Do you want extra cheese? Y or N ")
-        if Extra_cheese == "Y":
-            bill=large_pizza+1
-            print(f"Your total bill is: Rs{bill}")
-        else:
-            bill=large_pizza
-            print(f"Your total bill is: Rs{bill}")
-
-
+    bill = 25
 else:
     print("Invalid input")
+
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    elif size == "M" or size == "L":
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your total bill is: ${bill}")

93fa89e16426be936b5a6daec25245a0bbb620a8
Venkat <guptamaddula9@gmail.com>
2026-08-01 17:06:18 +0530

    added decrypt function to caeser cipher

--- python/caeser_cipher.py
+++ python/caeser_cipher.py
@@ -18,7 +18,19 @@ def encrypt(text,shift):
             
     print(f"The encoded text is {cipher_text}")
 
-
+def decrypt(text,shift):
+    decipher_text=""
+    for char in text:
+        if char in alphabet:
+            position=alphabet.index(char)
+            new_position=(position-shift)
+            if new_position<0:
+                new_position=new_position+26
+                decipher_text+=alphabet[new_position]
+            else:
+                decipher_text+=alphabet[new_position]
+            
+    print(f"The decoded text is {decipher_text}")
 
 
 #decrypt(text,shift)

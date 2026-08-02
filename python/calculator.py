6211f54e3aaf488cf4b93dc7226da717425f2481
Venkat <guptamaddula9@gmail.com>
2026-08-02 22:55:50 +0530

    added while loop to continue to continue calculations until user decides to exit and if else statement to perform calculations based on user input

--- python/calculator.py
+++ python/calculator.py
@@ -1,7 +1,3 @@
-num1 = float(input("Enter the first number: "))
-operation = input("Enter the operation (+, -, *, /): ")
-num2 = float(input("Enter the second number: "))
-
 def add(num1, num2):
     return num1 + num2
 
@@ -16,4 +12,24 @@ def divide(num1, num2):
         raise ValueError("Cannot divide by zero.")
     return num1 / num2
 
-#if operation == "+":
+do_perform=False
+num1 = float(input("Enter the first number: "))
+while not do_perform:
+    
+    operation = input("Enter the operation (+, -, *, /): ")
+    num2 = float(input("Enter the second number: "))
+    if operation == "+":
+        perform_operation = add
+    elif operation == "-":
+        perform_operation = subtract
+    elif operation == "*":
+        perform_operation = multiply
+    elif operation == "/":
+        perform_operation = divide
+
+    result = perform_operation(num1, num2)
+    print(f"The result of {num1} {operation} {num2} is: {result}")
+    num1=result
+    continue_calculation = input("Type 'yes' to continue calculating with the result, or 'no' to exit: ").lower()
+    if continue_calculation != "yes":
+        do_perform = True

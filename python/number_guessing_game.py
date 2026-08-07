e4942e4ffe7dd225f1a9821e9ddb1ca898e5d83e
Venkat <guptamaddula9@gmail.com>
2026-08-07 13:55:24 +0530

    added game level selection feature to the number guessing game. Players can now choose between easy, medium, and hard levels, which determine the number of attempts they have to guess the number.

--- python/number_guessing_game.py
+++ python/number_guessing_game.py
@@ -2,11 +2,18 @@ import random
 number_to_guess = random.randint(1, 100)
 print("Welcome to the Number Guessing Game!")
 print(number_to_guess)
+guessed_number = 0
+game_level=input("Choose a game level (easy, medium, hard): ")
+if game_level=="easy":
+    number_of_attempts=10
+elif game_level=="medium":
+    number_of_attempts=7
+else:
+    number_of_attempts=5
 
-number_of_attempts=10
-while number_of_attempts>=1:
+while number_of_attempts>=1 and guessed_number!=number_to_guess:
+    print("You have", number_of_attempts, "attempts to guess the number.")
     guessed_number=int(input("Guess a number between 1 and 100: "))
-    number_of_attempts-=1
     if guessed_number==number_to_guess:
         print("You guessed it right!")
     elif guessed_number-number_to_guess<=5 and number_to_guess-guessed_number<=5:
@@ -16,4 +23,5 @@ while number_of_attempts>=1:
     elif number_to_guess-guessed_number>5:
         print("You are too low!")
     number_of_attempts-=1
-    print(f"You have {number_of_attempts} attempts left")
+    
+    

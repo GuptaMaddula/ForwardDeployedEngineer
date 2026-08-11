fc676400531e7d6f114d5395565e150c362a4b55
Venkat <guptamaddula9@gmail.com>
2026-08-11 14:21:04 +0530

    added menu to the coffee machine

---
+++ python/coffee_machine.py
@@ -0,0 +1,26 @@
+menu={
+    "espresso": {
+        "ingredients": {
+            "milk": "100ml",
+            "water": "50ml",
+            "coffee": "18g",
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "milk": "150ml",
+            "water": "200ml",
+            "coffee": "24g",
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "milk": "100ml",
+            "water": "250ml",
+            "coffee": "24g",
+        },
+        "cost": 3.0,
+    }
+}

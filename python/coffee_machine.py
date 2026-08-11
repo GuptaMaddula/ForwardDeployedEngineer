9c97f665cfe37a87077b6705b19080d4900e3363
Venkat <guptamaddula9@gmail.com>
2026-08-11 14:24:32 +0530

    changed strings to numbers for easy calculation

--- python/coffee_machine.py
+++ python/coffee_machine.py
@@ -1,25 +1,25 @@
 menu={
     "espresso": {
         "ingredients": {
-            "milk": "100ml",
-            "water": "50ml",
-            "coffee": "18g",
+            "milk": 100,
+            "water": 50,
+            "coffee": 18,
         },
         "cost": 1.5,
     },
     "latte": {
         "ingredients": {
-            "milk": "150ml",
-            "water": "200ml",
-            "coffee": "24g",
+            "milk": 150,
+            "water": 200,
+            "coffee": 24,
         },
         "cost": 2.5,
     },
     "cappuccino": {
         "ingredients": {
-            "milk": "100ml",
-            "water": "250ml",
-            "coffee": "24g",
+            "milk": 100,
+            "water": 250,
+            "coffee": 24,
         },
         "cost": 3.0,
     }

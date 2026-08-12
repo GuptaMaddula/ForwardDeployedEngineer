e54ddadf553cd9eae10c383277b611e9807495e8
Venkat <guptamaddula9@gmail.com>
2026-08-12 14:35:07 +0530

    added functionality for calculating money based on the inserted coins

--- python/coffee_machine.py
+++ python/coffee_machine.py
@@ -24,3 +24,21 @@ menu={
         "cost": 3.0,
     }
 }
+
+report={
+    "milk": 300,
+    "water": 300,
+    "coffee": 100,
+    "money": 0
+}
+
+quarters=float(input("How many quarters? "))
+dimes=float(input("How many dimes? "))
+nickels=float(input("How many nickels? "))
+pennies=float(input("How many pennies? "))
+
+def calculate_money(quarters, dimes, nickels, pennies):
+    total = (quarters * 0.25) + (dimes * 0.10) + (nickels * 0.05) + (pennies * 0.01)
+    return total
+
+#def prepare_latte()

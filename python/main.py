dbe98490a42bea0a2921a695dd79120b8f8ff708
Venkat <guptamaddula9@gmail.com>
2026-08-25 13:05:12 +0530

    added function for randomly walking turtle

--- python/main.py
+++ python/main.py
@@ -23,16 +23,40 @@ timmy=t.Turtle()
 
 colours=["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen", "Violet", "Yellow", "Blue", "Green", "Indigo", "Orange", "Red"]
 
-def draw_shape(num_sides):
-    angle=360/num_sides
-    for _ in range(num_sides):
-        timmy.forward(100)
-        timmy.right(angle)
+def drawshapes():
+    
+    def draw_shape(num_sides):
+        angle=360/num_sides
+        for _ in range(num_sides):
+            timmy.forward(100)
+            timmy.right(angle)
 
 
-for shape_side_n in range(3,20):
-    timmy.shape("turtle")
-    timmy.color(random.choice(colours))
-    draw_shape(shape_side_n)
+    for shape_side_n in range(3,20):
+        timmy.shape("turtle")
+        timmy.color(random.choice(colours))
+        draw_shape(shape_side_n)
+
+def random_walk():
+    directions=[0,90,180,270]
+    timmy.pensize(20)
+
+
+    for _ in range(50):
+        timmy.shape("turtle")
+        timmy.color(random.choice(colours))
+        timmy.forward(50)
+        timmy.setheading(random.choice(directions))
+
+drawshapes()
+random_walk()
+
+
+
+
+
+
+
+
 
 

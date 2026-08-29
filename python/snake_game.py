174dce79a9cf56f2b7d6fd1f81119544bd638a27
Venkat <guptamaddula9@gmail.com>
2026-08-29 23:29:07 +0530

    added functionality to move snake forward

--- python/snake_game.py
+++ python/snake_game.py
@@ -1,17 +1,39 @@
 from turtle import Turtle, Screen
+import time
 
 screen=Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-x_positions=[0, -20, -40]
+starting_positions=[(0,0), (-20,0), (-40,0)]
+timmies=[]
 
-
-for position in range(0, 3):
-    tim=Turtle(shape="square")
+for position in starting_positions:
+    tim=Turtle("square")
     tim.color("white")
-    tim.goto(x=x_positions[position], y=0)
+    tim.penup()
+    tim.goto(position)
+    timmies.append(tim)
+
+game_is_on=True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+
+
+
+    for seg_num in range(len(timmies)-1, 0, -1):
+        new_x=timmies[seg_num-1].xcor()
+        new_y=timmies[seg_num-1].ycor()
+        timmies[seg_num].goto(new_x, new_y)
+
+    timmies[0].forward(20)
+
+
+               
 
 
 

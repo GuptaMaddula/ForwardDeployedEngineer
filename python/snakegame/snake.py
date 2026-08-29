62a958ac1bfdf29f12c320c9fc90855d9b4f8777
Venkat <guptamaddula9@gmail.com>
2026-08-30 00:07:53 +0530

    Added functionality to listen the keystrokes for snake to move

--- python/snakegame/snake.py
+++ python/snakegame/snake.py
@@ -2,7 +2,11 @@ from turtle import Turtle
 
 #constants should be in caps
 STARTING_POSITIONS=[(0,0), (-20,0), (-40,0)]
-MOVE_FORWARD=20
+MOVE_DISTANCE=20
+UP=90
+DOWN=270
+LEFT=180
+RIGHT=0
 
 class Snake:
     
@@ -26,4 +30,21 @@ class Snake:
             new_y=self.timmies[seg_num-1].ycor()
             self.timmies[seg_num].goto(new_x, new_y)
 
-        self.timmies[0].forward(MOVE_FORWARD)
+        self.timmies[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.timmies[0].heading()!=DOWN:
+            self.timmies[0].setheading(UP)
+
+    def down(self):
+        if self.timmies[0].heading()!=UP:
+            self.timmies[0].setheading(DOWN)
+
+    def left(self):
+        if self.timmies[0].heading()!=RIGHT:
+            self.timmies[0].setheading(LEFT)
+
+
+    def right(self):
+        if self.timmies[0].heading()!=LEFT:
+            self.timmies[0].setheading(RIGHT)
